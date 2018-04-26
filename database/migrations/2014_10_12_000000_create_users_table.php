f75726f5c136db0a17dec88d665c4f8056a569eb
Eugene Burdeinyi <ojdgaf@gmail.com>
2018-04-26 19:13:40 +0300

    migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,29 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('type');
             $table->string('email')->unique();
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
+            $table->enum('sex', ['Male', 'Female']);
+            $table->date('birthday')->nullable();
+            $table->string('passport')->nullable();
+            $table->string('residence')->nullable();
+            $table->string('phone')->nullable();
+
+            # Patient part
+            $table->unsignedInteger('diagnostic_card_id')->nullable();
+
+            # Employee part
+            $table->dateTime('hired_at')->nullable();
+            $table->dateTime('fired_at')->nullable();
+            $table->boolean('is_present')->nullable();
+            $table->text('about')->nullable();
+
+            # Doctor part
+            $table->string('degree')->nullable();
+
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ database/migrations/2018_04_26_183027_create_tests_table.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('diagnostic_card_id');
+            $table->timestamps();
+            $table->float('D2');
+            $table->float('D3');
+            $table->float('D4');
+            $table->float('D5');
+            $table->float('D6');
+            $table->float('D8');
+            $table->float('D11');
+            $table->float('D15');
+            $table->float('D20');
+            $table->float('D26');
+            $table->float('D36');
+            $table->float('D40');
+            $table->float('D65');
+            $table->float('D85');
+            $table->float('D120');
+            $table->float('D150');
+            $table->float('D210');
+            $table->float('D290');
+            $table->float('D300');
+            $table->float('D520');
+            $table->float('D700');
+            $table->float('D950');
+            $table->float('D1300');
+            $table->float('D1700');
+            $table->float('D2300');
+            $table->float('D3100');
+            $table->float('D4200');
+            $table->float('D5600');
+            $table->float('D7600');
+            $table->float('D10200');
+            $table->float('D13800');
+            $table->float('D18500');
+
+            $table->foreign('diagnostic_card_id')
+                ->references('id')->on('diagnostic_cards')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
