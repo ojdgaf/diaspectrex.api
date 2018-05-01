0f2fd8ef3932250d63f71fd3c44b05b17de1aec9
Eugene Burdeinyi <ojdgaf@gmail.com>
2018-05-01 17:43:19 +0300

    JWT authentication

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -2,18 +2,21 @@
 
 namespace App\Models\Users;
 
+use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Nanigans\SingleTableInheritance\SingleTableInheritanceTrait;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use Notifiable, HasRoles, SingleTableInheritanceTrait;
 
     protected $table = 'users';
 
     protected static $singleTableTypeField = 'type';
+
+    protected static $singleTableType = 'user';
 
     protected static $singleTableSubclasses = [Patient::class, Employee::class];
 
@@ -24,4 +27,24 @@ class User extends Authenticatable
         'first_name', 'middle_name', 'last_name',
         'birthday', 'passport', 'residence',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
-Auth::routes();
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-//Route::group(['middleware' => ['role:support']], function () {
-    Route::resource('doctors', 'Users\DoctorController');
+//Auth::routes();
+//
+//Route::get('/', function () {
+//    return view('welcome');
 //});
-
-Route::prefix('tests')->group(function () {
-    Route::prefix('uploading')->group(function () {
-        Route::post('manually', 'Diagnosing\TestController@createTestManually');
-        Route::post('from-file', 'Diagnosing\TestController@createTestFromFile');
-    });
-});
+//
+//Route::get('/home', 'HomeController@index')->name('home');
+//
+////Route::group(['middleware' => ['role:support']], function () {
+//    Route::resource('doctors', 'Users\DoctorController');
+////});
+//
+//Route::prefix('tests')->group(function () {
+//    Route::prefix('uploading')->group(function () {
+//        Route::post('manually', 'Diagnosing\TestController@createTestManually');
+//        Route::post('from-file', 'Diagnosing\TestController@createTestFromFile');
+//    });
+//});
