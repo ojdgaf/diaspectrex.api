784bb03972590f0f58a86256e6210eb7cabddf81
Eugene Burdeinyi <ojdgaf@gmail.com>
2018-05-11 20:09:07 +0300

    column ordering in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,25 +15,25 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->unsignedInteger('address_id')->nullable();
             $table->string('email')->unique();
             $table->string('first_name');
             $table->string('middle_name')->nullable();
             $table->string('last_name');
             $table->enum('sex', ['male', 'female']);
             $table->date('birthday');
+            $table->unsignedInteger('address_id')->nullable();
 
             # Employee part
             $table->string('passport')->nullable();
             $table->boolean('is_present')->nullable();
             $table->text('about')->nullable();
+            $table->dateTime('hired_at')->nullable();
+            $table->dateTime('fired_at')->nullable();
 
             # Doctor part
             $table->string('degree')->nullable();
 
             $table->string('password');
-            $table->dateTime('hired_at')->nullable();
-            $table->dateTime('fired_at')->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
