896be2bcded5e67ae34788adbbb5f7a6f77e20b3
Eugene Burdeinyi <ojdgaf@gmail.com>
2018-05-13 03:06:12 +0300

    base controller docs

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,9 +7,18 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * Class Controller
+ * @package App\Http\Controllers
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
+    /**
+     * Quantity of location items
+     * (countries / regions / cities / streets / addresses)
+     * per response.
+     */
     const LOCATION_PAGINATION = 100;
 }
