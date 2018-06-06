bca8c657f9c328bab26185606b5b2c79bd563737
Eugene Burdeinyi <ojdgaf@gmail.com>
2018-06-06 19:30:30 +0300

    services

--- app/Http/Resources/Service.php
+++ app/Http/Resources/Service.php
@@ -9,11 +9,16 @@ class Service extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'          => $this->id,
+            'name'        => $this->name,
+            'description' => $this->description,
+            'price'       => $this->price,
+        ];
     }
 }
